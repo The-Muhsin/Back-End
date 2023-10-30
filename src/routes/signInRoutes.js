2a06511c0ff2828f54f5f15bba3d5673dc9f6a4f
Abdulquddus Ibrahim <ibrahimabdulquddus51@gmail.com>
2023-10-30 21:23:21 +0100

    bug fix

--- src/routes/signInRoutes.js
+++ src/routes/signInRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const signInController = require('../controllers/signInController');
+const signInController = require('../controller/signInController');
 
 // Define the route for user sign-in
 router.post('/signin', signInController);

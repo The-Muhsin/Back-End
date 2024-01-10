c9d8e95f927fa97db86f2535e6839641bce78f76
Abdulhaleem <nabdulhaleem09@gmail.com>
2024-01-10 04:48:16 +0100

    ignored node_modules and also added services

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-// config.js
-
-module.exports = {
-  DB_URI: "mongodb+srv://themuhsinweb:Gm0MPi0JqKmBdDxR@themuhsin.y1glkqn.mongodb.net/?retryWrites=true&w=majority",
-  PORT: 5000, 
-  GOOGLE_CLIENT_ID: "476602469967-o0umc8le1eu7bvd91nufvm0dhreocpto.apps.googleusercontent.com",
-  GOOGLE_CLIENT_SECRET: "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",
-};
